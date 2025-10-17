e053f7a03bd7372971ec5df9d6ccdecc052584ad
Wahaj Ameen <wahajmaqsood20@gmail.com>
2025-10-17 16:11:27 +0400

    add get and post

--- core/client.go
+++ core/client.go
@@ -1,6 +1,13 @@
 package core
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+)
 
 type Client struct {
 	headers map[string]string
@@ -19,4 +26,57 @@ func NewClient(appName string, appVersion string, apiKey string, apiVersion int)
 		"https",
 		"api.trakt.tv",
 	}
+}
+
+func (c *Client) constructUrl(path string, queryParams map[string]any) string {
+	u := url.URL{
+		Scheme: c.schema,
+		Host: c.baseUrl,
+		Path: path,
+	}
+	qParams := url.Values{}
+	for k, v := range queryParams {
+		qParams.Set(k, fmt.Sprintf("%s", v))
+	}
+	u.RawQuery = qParams.Encode()
+	return u.String()
+}
+
+func (c *Client) do(method, path string, queryParams, payload map[string]any) (map[string]any, error) {
+	serializedPayload, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+	payloadBuffer := bytes.NewBuffer(serializedPayload)
+
+	req, err := http.NewRequest(method, c.constructUrl(path, queryParams), payloadBuffer)
+	if err != nil {
+		return nil, err
+	}
+	for k, v := range c.headers {
+		req.Header.Set(k, v)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	responseBuffer, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	responseBody := make(map[string]any)
+	json.Unmarshal(responseBuffer, &responseBody)
+
+	return responseBody, nil
+}
+
+func (c *Client) Get(path string, queryParams map[string]any) (map[string]any, error) {
+	return c.do(http.MethodGet, path, queryParams, nil)
+}
+
+func (c *Client) Post(path string, queryParams, payload map[string]any) (map[string]any, error) {
+	return c.do(http.MethodPost, path, queryParams, payload)
 }
