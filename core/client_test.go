f7b16a1f5549b44bf02acdc16b91085d24e372de
Wahaj Ameen <wahajmaqsood20@gmail.com>
2025-10-26 17:59:19 +0400

    test get

--- core/client_test.go
+++ core/client_test.go
@@ -1,6 +1,10 @@
 package core
 
 import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -20,4 +24,32 @@ func TestMakeClient(t *testing.T) {
 	)
 	assert.Equal(t, "https", client.scheme)
 	assert.Equal(t, "api.trakt.tv", client.baseUrl)
+}
+
+func NewTestClientServer(handler http.HandlerFunc) (*Client, *httptest.Server) {
+	server := httptest.NewServer(handler)
+	traktBaseUrl = strings.TrimPrefix(server.URL, "http://")
+	traktScheme = "http"
+	client := NewClient("test", "test", "test", 2)
+	return client, server
+}
+
+func TestGet(t *testing.T) {
+	path := "/path"
+	queryParams := map[string]any{"q1": "v1"}
+	response := map[string]any{"r1": "v1"}
+	client, server := NewTestClientServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method)
+		assert.Equal(t, path, r.URL.Path)
+		assert.Equal(t, "q1=v1", r.URL.RawQuery)
+		w.WriteHeader(http.StatusOK)
+		marshaled, _ := json.Marshal(response)
+		w.Write([]byte(marshaled))
+	}))
+	defer server.Close()
+
+	resp, err := client.Get(path, queryParams)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, resp.StatusCode)
+	assert.Equal(t, response, resp.Body)
 }
