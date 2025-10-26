cfefadf01ef8dc506006d8e9ba88bc7ca23f1b3c
Wahaj Ameen <wahajmaqsood20@gmail.com>
2025-10-26 18:09:10 +0400

    test post

--- core/client_test.go
+++ core/client_test.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 	"testing"
 
+	"trakt-go/util"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -49,6 +51,30 @@ func TestGet(t *testing.T) {
 	defer server.Close()
 
 	resp, err := client.Get(path, queryParams)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, resp.StatusCode)
+	assert.Equal(t, response, resp.Body)
+}
+
+func TestPost(t *testing.T) {
+	path := "/path"
+	queryParams := map[string]any{"q1": "v1"}
+	payload := map[string]any{"b1": "v1"}
+	response := map[string]any{"r1": "v1"}
+	client, server := NewTestClientServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPost, r.Method)
+		assert.Equal(t, path, r.URL.Path)
+		assert.Equal(t, "q1=v1", r.URL.RawQuery)
+		deserializedBody, err := util.Deserialize(r.Body)
+		assert.Nil(t, err)
+		assert.Equal(t, payload, deserializedBody)
+		w.WriteHeader(http.StatusOK)
+		marshaled, _ := json.Marshal(response)
+		w.Write([]byte(marshaled))
+	}))
+	defer server.Close()
+
+	resp, err := client.Post(path, queryParams, payload)
 	assert.Nil(t, err)
 	assert.Equal(t, 200, resp.StatusCode)
 	assert.Equal(t, response, resp.Body)
